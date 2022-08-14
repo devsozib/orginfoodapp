28d25f6109c34ede1e636c0c34aa70a29b0a7c91
shawonUU <shawonmahmodul12@gmail.com>
2022-08-14 14:08:41 +0600

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
+            $table->string('name');
             $table->boolean('is_deleted')->default(0);
             $table->rememberToken();
             $table->timestamps();
