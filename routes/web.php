3decd85e0de70e643710c5a2be5db56dbf5d296d
devsozib <mdsajibhassa01993@gmail.com>
2022-08-16 09:45:47 +0600

    branch senction

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function (){
 
-
-
-
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/', function () {
         return view('home');
     });
-
-
-
 
 });
 
